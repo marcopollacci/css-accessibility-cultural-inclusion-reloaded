5543ba290e14de4841e3c2e7dc0ac8fdfc40a003
Marco Pollacci <m.pollacci@40-factory.com>
2025-11-13 11:05:44 +0100

    fix(project): :bug: fix js replace

--- src/js/fonts.js
+++ src/js/fonts.js
@@ -1,9 +1,11 @@
 const replaceNoLatin = () => {
-  Array.from(document.getElementsByClassName('no-replace-font')).forEach(el => {
-    const originalText = el.innerText;
-    const modifiedText = originalText.replace(/[^\x00-\x7F]/g, '�');
-    el.innerText = modifiedText;
-  });
+  Array.from(document.getElementsByClassName("no-replace-font")).forEach(
+    (el) => {
+      const originalText = el.textContent;
+      const modifiedText = originalText.replace(/[^\x00-\x7F]/g, "�");
+      el.innerText = modifiedText;
+    }
+  );
 };
 
 replaceNoLatin();
